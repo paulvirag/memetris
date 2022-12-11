82700865bc1530a9f1aea8cb279b0c60af45aefc
Paul Virag <paulvirag@live.com>
2022-12-11 01:57:20 -0800

    [Sockets] Set up sockets.io

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const PORT = process.env.PORT || 5001;
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app
   .use('/index', express.static('views/index.html'))
   .get('/', (req, res, next) => res.redirect('/index'))
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Server started on localhost:${PORT}`));
+  .use(express.static(path.join(__dirname, 'public')));
+
+server.listen(PORT, () => console.log(`Server started on localhost:${PORT}`));
+
+io.on('connection', socket => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
